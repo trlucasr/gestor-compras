d09ceb36d6ae32c2d64b94574789861179421262
Lucas Ribeiro <79470623+trlucasr@users.noreply.github.com>
2022-01-27 15:35:42 -0300

    fix app

--- gestor.py
+++ gestor.py
@@ -119,6 +119,6 @@ def logout():
     flash('Acesse novamente para utilizar o App')
     return redirect(url_for('index'))
 
-
-app.run(host="0.0.0.0", port=5001)
+if __name__ == '__main__':
+    app.run()
 
